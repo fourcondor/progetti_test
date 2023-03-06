144710518c2a87c8664832ca6f0738ff5cc1bc72
fourcondor <s308494@studenti.polito.it>
2023-03-06 21:05:29 +0100

    primo prova2 commit

---
+++ prova2/target/debug/deps/prova2.d
@@ -0,0 +1,5 @@
+C:\Users\gbl98\CLionProjects\progetti_test\prova2\target\debug\deps\prova2.exe: src\main.rs
+
+C:\Users\gbl98\CLionProjects\progetti_test\prova2\target\debug\deps\prova2.d: src\main.rs
+
+src\main.rs:
---
+++ prova2/target/debug/prova2.d
@@ -0,0 +1,1 @@
+C:\Users\gbl98\CLionProjects\progetti_test\prova2\target\debug\prova2.exe: C:\Users\gbl98\CLionProjects\progetti_test\prova2\src\main.rs
